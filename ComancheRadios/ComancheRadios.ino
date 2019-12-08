10603cd2cac1c09aaa9fcb9e6e2622783fdedd25
Alexey Ignatenko <alexey.ignatenko@gmail.com>
2019-12-09 01:46:40 +0300

    added buttons handling

--- ComancheRadios/ComancheRadios.ino
+++ ComancheRadios/ComancheRadios.ino
@@ -1,12 +1,12 @@
+#include <Encoder.h>
+#include <Bounce2.h>
 
 #define SIM
 
 #ifdef SIM
 #include <si_message_port.hpp>
-
 SiMessagePort* messagePort;
 #endif
-
 
 #include "LiquidCrystal_PCF8574.h"
 
@@ -30,25 +30,55 @@ SiMessagePort* messagePort;
 //  A1 - 4 -- port 41
 //  A2 - 6 -- port 42
 
-
 enum Messages  {
   REFRESH_STATE = 0, // payload: none
   ENABLE_DISPLAY_BACKLIGHT = 1, // payload: <int 0 (left), 1 (right)> <int 0 (disable), 1 (enable)>
   SET_DISPLAY_TEXT = 2, // payload: <int 0 (left), 1 (right)> <int 0 (line 1), 1 (line 2)> || <string text>
+  KNOB_UPDATED = 3, // payload <int 0 (left), 1 (right) > <int 0 (inner), int 1 (outer)> <int -1 - left, 1 right>
+  TOGGLE_PRESSED = 4, // payload <int 0 (left), 1 (right)> <int 0 released, 1 pressed>
+  ONOFF_PRESSED = 5, // payload <int 0 (left), 1 (right)> <int 0 released, 1 pressed>
 };
 
+// setup encoders
+
+int32_t knobs_positions[2][2] = {0};
+
+Encoder* knobs[2][2] = {
+  {new Encoder(14, 15), new Encoder(23, 24)},
+  {new Encoder(32, 33), new Encoder(41, 42)}
+};
+
+// setup buttons
+
+Bounce* createButton(int pin) {
+  pinMode(pin, INPUT_PULLUP);
+  Bounce* btn = new Bounce();
+  btn->attach(pin);
+  btn->interval(5); // interval in ms
+  return btn;
+};
+
+Bounce* btns_toggle[2] = {
+  createButton(36),
+  createButton(18),
+};
+
+Bounce* btns_onoff[2] = {
+  createButton(44),
+  createButton(26),
+
+};
 
 // A4 as SDA, A5 as SCL
 LiquidCrystal_PCF8574* lcds[2];
 
 void set_backlight(int dspl, bool enable) {
-   lcds[dspl]->setBacklight(enable ? 255 : 0);
+  lcds[dspl]->setBacklight(enable ? 255 : 0);
 }
 
-
 void set_display_text(int dspl, int line, const char* text) {
-    lcds[dspl]->setCursor(0, line);
-    lcds[dspl]->print(text);
+  lcds[dspl]->setCursor(0, line);
+  lcds[dspl]->print(text);
 }
 
 #ifdef SIM
@@ -62,7 +92,7 @@ static void new_message_callback(uint16_t message_id, struct SiMessagePortPayloa
   // The arguments are only valid within this function!
   // Make a clone if you want to store it
   messagePort->DebugMessage(SI_MESSAGE_PORT_LOG_LEVEL_INFO, (String)"Received something with id " + message_id);
-  
+
   switch (message_id)
   {
     case REFRESH_STATE: break;
@@ -71,7 +101,7 @@ static void new_message_callback(uint16_t message_id, struct SiMessagePortPayloa
         int enable = payload->data_int[1];
         set_backlight(dspl, enable);
         messagePort->DebugMessage(SI_MESSAGE_PORT_LOG_LEVEL_INFO, (String)"Enable display " + dspl + " " + enable);
- 
+
         break;
       }
     case SET_DISPLAY_TEXT: {
@@ -122,11 +152,64 @@ void setup()
 
   }
 
+  // setup knobs
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      knobs_positions[i][j] = knobs[i][j]->read() / 2;
+    }
+  }
 } // setup()
 
 
 void loop()
 {
+  // updat knobs
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      int32_t new_pos = knobs[i][j]->read() / 2;
+      if (new_pos != knobs_positions[i][j]) {
+        //Serial.print(String("knob ") + i + " " + j + " new pos: " + new_pos);
+        //Serial.println();
+#ifdef SIM
+        bool cw = (new_pos > knobs_positions[i][j]);
+        int32_t payload[3] = { i, j,  cw ? 1 : -1};
+        messagePort->SendMessage(KNOB_UPDATED, payload, 3);
+
+#endif
+        knobs_positions[i][j] = new_pos;
+
+      }
+    }
+  }
+
+  // updfate buttons
+
+  // check buttons for swap
+  for (int i = 0; i < 2; ++i) {
+    bool updated = btns_toggle[i]->update();
+    if (updated) {
+      int32_t payload[2] = {i, !btns_toggle[i]->read()};
+#ifdef SIM
+      messagePort->SendMessage(TOGGLE_PRESSED, payload, 2);
+#else
+      Serial.print(i);
+#endif
+    }
+  }
+
+// check buttons for on/off
+  for (int i = 0; i < 2; ++i) {
+    bool updated = btns_onoff[i]->update();
+    if (updated) {
+      int32_t payload[2] = {i, !btns_onoff[i]->read()};
+#ifdef SIM
+      messagePort->SendMessage(ONOFF_PRESSED, payload, 2);
+#else
+      Serial.print(i);
+#endif
+    }
+  }
+
 #ifdef SIM
   // Make sure this function is called regularly
   messagePort->Tick();
